73f709500bd296c2ffe9393627c52698de48262a
nnabighods <n.nabighods@gmail.com>
2022-03-10 17:01:43 -0700

    a

--- src/components/appbar/AppBar.js
+++ src/components/appbar/AppBar.js
@@ -35,20 +35,21 @@ function AppBar(props) {
         <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3">
             <Nav.Link href="/dashboard" className=" d-flex gap-2"><Icon.Home/>Home</Nav.Link>
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.File/>Reports</Nav.Link>
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.Clipboard/>Orders</Nav.Link>
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.ShoppingCart/>Products</Nav.Link>
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.Users/>Customers</Nav.Link>
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.TrendingUp/>Overview</Nav.Link>
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.BookOpen/>Sale History</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.File/>Reports</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.Clipboard/>Orders</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.ShoppingCart/>Products</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.Users/>Customers</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.TrendingUp/>Overview</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.BookOpen/>Sale History</Nav.Link>
             <h6 className="mt-4 mb-1 text-muted h6">Product Info</h6>
             {/* <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.Plus/>Add Product</Nav.Link> */}
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.Smile/>Brands</Nav.Link>
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.Star/>Reviews</Nav.Link>
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.Grid/>Categories</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.Smile/>Brands</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.Star/>Reviews</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.Grid/>Categories</Nav.Link>
             <h6 className="mt-4 mb-1 text-muted h6">Account</h6>
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.CheckCircle/>Authentication</Nav.Link>
-            <Nav.Link eventKey="#" className=" d-flex gap-2"><Icon.User/>Users</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.CheckCircle/>Authentication</Nav.Link>
+            <Nav.Link eventKey="#" className="d-flex gap-2"><Icon.User/>Users</Nav.Link>
+            <Link to="/" className="d-flex gap-2 text-muted mt-4"><Icon.LogOut/>Logout</Link>
             </Nav>
         </Offcanvas.Body>
         </Navbar.Offcanvas>
